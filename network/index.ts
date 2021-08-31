0d7744f8d059334f6e3ff68dc87b30e17bb305b2
karbafoo <karbafooo@gmail.com>
2021-08-31 21:46:34 +0430

    .3

--- network/index.ts
+++ network/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://157.90.19.32:8081/api/';
-// const BASE_URL = 'http://localhost:8081/api/';
+// const BASE_URL = 'http://157.90.19.32:8081/api/';
+const BASE_URL = 'http://localhost:8081/api/';
 const getFetcher = (url) => axios.get(BASE_URL + url).then((res) => res.data);
 const postReqHandler = (url, data) =>
     axios({
